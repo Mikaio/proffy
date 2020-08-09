57da8c242a5fce58e6940594a77b0834ca59051c
Mikaio Melo <mikaiomelo23@gmail.com>
2020-08-09 10:07:44 -0300

    Add classes controller

---
+++ server/src/controllers/ClassesController.ts
@@ -0,0 +1,102 @@
+import { Request, Response } from 'express'
+
+import db from '../database/connection';
+import convertHourToMinutes from '../utils/convertHourToMinutes';
+
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
+
+export default class ClassesController {
+    // List teachers
+    async index(request: Request, response: Response) {
+        const filters = request.query;
+
+        const subject = filters.subject as string;
+        const week_day = filters.week_day as string;
+        const time = filters.time as string;
+
+        if(!filters.week_day || !filters.subject || !filters.time) {
+            return response.status(400).json({
+                error: 'Missing filters to search classes'
+            })
+        }
+
+        const timeInMinutes = convertHourToMinutes(time);
+
+        const classes = await db('classes')
+            .whereExists(function() {
+                this.select('class_schedule.*')
+                    .from('class_schedule')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                    .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                    .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+            })
+            .where('classes.subject', '=', subject)
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users.*']);
+
+        response.json(classes);
+    }
+
+    // Add a new teacher
+    async create(request: Request, response: Response) {
+        const {
+           name,
+           avatar,
+           whatsapp,
+           bio,
+           subject,
+           cost,
+           schedule 
+        } = request.body;
+    
+        const trx = await db.transaction(); 
+        // All the db actions will be done at the same time and
+        // if one goes wrong all insertions will be canceled
+    
+        try {
+            const insertedUsersIds = await trx('users').insert({
+                name,
+                avatar,
+                whatsapp,
+                bio,
+            });
+        
+            const user_id = insertedUsersIds[0];
+        
+            console.log("ID:", user_id)
+        
+            const insertedClassesIds = await trx('classes').insert({
+                subject,
+                cost,
+                user_id,
+            });
+        
+            const class_id = insertedClassesIds[0];
+        
+            const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+                return {
+                    class_id,
+                    week_day: scheduleItem.week_day,
+                    from: convertHourToMinutes(scheduleItem.from),
+                    to: convertHourToMinutes(scheduleItem.to),
+                }
+            });
+        
+            await trx('class_schedule').insert(classSchedule);
+        
+            await trx.commit(); // If everything goes right, the insertions will be done
+        
+            return response.status(201).send();
+        } catch (err) {
+            await trx.rollback();
+            return response.status(400).json({
+                error: 'Unexpected error while creating new class'
+            });
+        }
+    }
+}
