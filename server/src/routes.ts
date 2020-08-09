a5008547870dd14d6825e9cd4358ede6c3c87fe8
Mikaio Melo <mikaiomelo23@gmail.com>
2020-08-09 10:06:45 -0300

    Add routes file

---
+++ server/src/routes.ts
@@ -0,0 +1,21 @@
+import express, { request, response } from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
+const routes = express.Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
+
+routes.get('/', (request, response) => {
+    console.log('Testing');
+
+    return response.send("Success");
+})
+
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
+
+export default routes;
