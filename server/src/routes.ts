e4bc3724abd7e4a3e84f8b90ec1e9d28c111348d
Mikaio Melo <mikaiomelo23@gmail.com>
2020-08-12 19:35:01 -0300

    Feature(login/signup): Add new users' routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,11 @@
 import express, { request, response } from 'express';
 import ClassesController from './controllers/ClassesController';
 import ConnectionsController from './controllers/ConnectionsController';
+import UsersController from './controllers/UsersController';
 
 const routes = express.Router();
+
+const usersController = new UsersController();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
 
@@ -11,6 +14,9 @@ routes.get('/', (request, response) => {
 
     return response.send("Success");
 })
+
+routes.post('/login', usersController.index)
+routes.post('/signin', usersController.create);
 
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
